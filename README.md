1a72a6bc06f2bc19236135be93720190ddc11bc4
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2025-02-08 15:39:57 +0530

    updated some more patterns

--- README.md
+++ README.md
@@ -1,70 +1,23 @@
-# Getting Started with Create React App
+# 🚀 Programming Patterns
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+A web page that lists various programming patterns with problem links, solutions, and a checkbox to track completion. Built with **React** and hosted on **GitHub Pages**.
 
-## Available Scripts
+## 🌐 Live Demo
 
-In the project directory, you can run:
+🔗 [View the Live Project](https://hey-shashikant.github.io/Programming-Patterns)
 
-### `npm start`
+---
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## 📥 Cloning and Running Locally
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Follow these steps to set up the project on your local machine.
 
-### `npm test`
+### 1️⃣ Prerequisites
+Ensure you have **Node.js** and **npm** installed.  
+🔹 [Download Node.js](https://nodejs.org/) (npm comes bundled with Node.js)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+### 2️⃣ Clone the Repository
+Run the following command in your terminal:
+```sh
+git clone https://github.com/hey-shashikant/Programming-Patterns.git
+cd Programming-Patterns
--- src/data/PatternData.js
+++ src/data/PatternData.js
@@ -1,20 +1,51 @@
 const PatternData = [
   {
     id: 1,
-    name: "Sliding Window",
+    name: "Two Pointers",
     problems: [
-      { id: "p1", name: "Repeated DNA Sequences", link: "https://leetcode.com/problems/repeated-dna-sequences/", done: false, difficulty: "Hard" },
-      { id: "p2", name: "Sliding Window Maximum", link: "https://leetcode.com/problems/sliding-window-maximum/", done: false, difficulty: "Medium" },
-      { id: "p3", name: "Minimum Window Subsequence", link: "https://www.geeksforgeeks.org/problems/minimum-window-subsequence/", done: false, difficulty: "Hard" }
-    ]
+      { id: "p1", name: "Valid Palindrome", link: "https://leetcode.com/problems/valid-palindrome/", difficulty: "Easy", done: false },
+      { id: "p2", name: "3Sum", link: "https://leetcode.com/problems/3sum/description/", difficulty: "Medium", done: false },
+      { id: "p3", name: "Remove Nth Node From End of List", link: "https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/", difficulty: "Medium", done: false },
+      { id: "p4", name: "Sort Colors", link: "https://leetcode.com/problems/sort-colors/", difficulty: "Medium", done: false },
+      { id: "p5", name: "Reverse Words in a String", link: "https://leetcode.com/problems/reverse-words-in-a-string/description/", difficulty: "Medium", done: false },
+      { id: "p6", name: "Valid Word Abbreviation", link: "https://leetcode.com/problems/valid-word-abbreviation/description/", difficulty: "Easy", done: false },
+      { id: "p7", name: "Strobogrammatic Number", link: "https://leetcode.com/problems/strobogrammatic-number/", difficulty: "Easy", done: false },
+      { id: "p8", name: "Minimum Moves to Make Palindrome", link: "https://leetcode.com/problems/minimum-number-of-moves-to-make-palindrome/", difficulty: "Hard", done: false },
+      { id: "p9", name: "Next Palindrome Using Same Digits", link: "https://leetcode.com/problems/next-palindrome-using-same-digits/", difficulty: "Hard", done: false },
+      { id: "p10", name: "Lowest Common Ancestor of a Binary Tree III", link: "https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/", difficulty: "Medium", done: false },
+      { id: "p11", name: "Valid Palindrome II", link: "https://leetcode.com/problems/valid-palindrome-ii/description/", difficulty: "Easy", done: false },
+    ],
   },
   {
     id: 2,
     name: "Fast & Slow Pointers",
     problems: [
-      { id: "p4", name: "Happy Number", link: "https://leetcode.com/problems/happy-number/", done: false, difficulty: "Easy" },
-      { id: "p5", name: "Linked List Cycle", link: "https://leetcode.com/problems/linked-list-cycle/", done: false, difficulty: "Medium" }
-    ]
+      { id: "p12", name: "Happy Number", link: "https://leetcode.com/problems/happy-number/description/", difficulty: "Easy", done: false },
+      { id: "p13", name: "Linked List Cycle", link: "https://leetcode.com/problems/linked-list-cycle/description/", difficulty: "Medium", done: false },
+      { id: "p14", name: "Middle of the Linked List", link: "https://leetcode.com/problems/middle-of-the-linked-list/description/", difficulty: "Easy", done: false },
+      { id: "p15", name: "Circular Array Loop", link: "https://leetcode.com/problems/circular-array-loop/description/", difficulty: "Medium", done: false },
+      { id: "p16", name: "Find the Duplicate Number", link: "https://leetcode.com/problems/find-the-duplicate-number/description/", difficulty: "Medium", done: false },
+      { id: "p17", name: "Palindrome Linked List", link: "https://leetcode.com/problems/palindrome-linked-list/description/", difficulty: "Easy", done: false },
+      { id: "p18", name: "Maximum Twin Sum of a Linked List", link: "https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/", difficulty: "Medium", done: false },
+      { id: "p19", name: "Split a Circular Linked List into Two Halves", link: "https://www.geeksforgeeks.org/problems/split-a-circular-linked-list-into-two-halves/1", difficulty: "Medium", done: false },
+    ],
+  },
+  {
+    id: 3,
+    name: "Sliding Window",
+    problems: [
+      { id: "p20", name: "Repeated DNA Sequences", link: "https://leetcode.com/problems/repeated-dna-sequences/description/", difficulty: "Hard", done: false },
+      { id: "p21", name: "Sliding Window Maximum", link: "https://leetcode.com/problems/sliding-window-maximum/description/", difficulty: "Medium", done: false },
+      { id: "p22", name: "Minimum Window Subsequence", link: "https://www.geeksforgeeks.org/problems/minimum-window-subsequence/0", difficulty: "Hard", done: false },
+      { id: "p23", name: "Longest Repeating Character Replacement", link: "https://leetcode.com/problems/longest-repeating-character-replacement/description/", difficulty: "Medium", done: false },
+      { id: "p24", name: "Minimum Window Substring", link: "https://leetcode.com/problems/minimum-window-substring/", difficulty: "Hard", done: false },
+      { id: "p25", name: "Longest Substring Without Repeating Characters", link: "https://leetcode.com/problems/longest-substring-without-repeating-characters/", difficulty: "Medium", done: false },
+      { id: "p26", name: "Minimum Size Subarray Sum", link: "https://leetcode.com/problems/minimum-size-subarray-sum/description/", difficulty: "Medium", done: false },
+      { id: "p27", name: "Maximum Average Subarray I", link: "https://leetcode.com/problems/maximum-average-subarray-i/description/", difficulty: "Easy", done: false },
+      { id: "p28", name: "Diet Plan Performance", link: "https://leetcode.com/problems/diet-plan-performance/", difficulty: "Easy", done: false },
+      { id: "p29", name: "Fruit Into Baskets", link: "https://leetcode.com/problems/fruit-into-baskets/", difficulty: "Medium", done: false },
+      { id: "p30", name: "Contains Duplicate II", link: "https://leetcode.com/problems/contains-duplicate-ii/description/", difficulty: "Easy", done: false },
+    ],
   }
 ];
 
